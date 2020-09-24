efc69cbb067c821d264517213d71e4bd957f6646
Carlo Pascual <carlopascual@yahoo.com>
2020-09-24 20:33:47 +0200

    Add logo

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 import { HIGHLIGHT_COLOR } from "./styles";
+import Logo from "./logo";
 
 const Header = styled.header`
+  display: flex;
+  align-items: center;
   font-size: 15px;
   color: white;
-  padding: 16px;
+  padding: 12px 16px;
   background: ${HIGHLIGHT_COLOR};
 `;
 
@@ -15,7 +18,12 @@ const BodyWrapper = styled.div`
 
 export default ({ children }) => (
   <div>
-    <Header>Iceboks</Header>
+    <Header>
+      <div style={{ height: "24px", width: "24px" }}>
+        <Logo />
+      </div>
+      <div style={{ marginLeft: "8px" }}>Iceboks</div>
+    </Header>
     <BodyWrapper>{children}</BodyWrapper>
   </div>
 );
---
+++ components/logo.js
@@ -0,0 +1,33 @@
+export default () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <g fill="none" fill-rule="evenodd">
+      <g>
+        <path fill="#FFF" d="M0 0H1440V900H0z" transform="translate(-16 -12)" />
+        <g>
+          <path
+            fill="#66F"
+            d="M0 0H1440V48H0z"
+            transform="translate(-16 -12)"
+          />
+          <g>
+            <path
+              d="M0 0H24V24H0z"
+              transform="translate(-16 -12) translate(16 12)"
+            />
+            <path
+              fill="#FFF"
+              fill-rule="nonzero"
+              d="M21.3 4.6l-8-4c-.8-.4-1.8-.4-2.7 0l-8 4C1.6 5.1 1 6.1 1 7.2v9.5c0 1.1.6 2.2 1.7 2.7l8 4c.4.2.9.3 1.3.3.5 0 .9-.1 1.3-.3l8-4c1-.5 1.7-1.5 1.7-2.7V7.2c0-1.1-.6-2.1-1.7-2.6zm-9.7-2.3c.1-.1.3-.1.4-.1.2 0 .3 0 .4.1L19.8 6 12 9.9 4.2 6l7.4-3.7zM3.5 17.7c-.3-.2-.5-.6-.5-.9V7.6l8 4v9.8l-7.5-3.7zm16.9 0L13 21.4v-9.8l8-4v9.2c0 .4-.2.7-.6.9z"
+              transform="translate(-16 -12) translate(16 12)"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+);
